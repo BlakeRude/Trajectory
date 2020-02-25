141761cd83bf3528c2fc428137471e0b53f34463
BlakeRude <60417657+BlakeRude@users.noreply.github.com>
2020-02-25 10:52:56 -0800

    ...

---
+++ Trajectory/Assets/src/Max/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//insert KABOOM sound effect here
+public class SoundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // I like to smell fish in my free time
+    void Update()
+    {
+        
+    }
+}
