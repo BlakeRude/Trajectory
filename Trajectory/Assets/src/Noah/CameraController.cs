d17335278b981309e90bb99e8d1b5535471460f8
Noah Mammen <60499225+NoahVeil@users.noreply.github.com>
2020-02-26 17:58:20 -0800

    some movement added

---
+++ Trajectory/Assets/src/Noah/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 5.0f;
+    //private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    public float distance = 10.0f;
+    private float curX = 0;
+    //private float curY = 0;
+    public float sensitivityX = 0;
+    //private float sensitivityY = 0;
+ 
+    void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        curX += Input.GetAxis("Mouse X") * sensitivityX;
+        //curY += Input.GetAxis("Mouse Y");
+
+        //curY = Mathf.Clamp(curY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    //waits for player movement
+    void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(30.0f, curX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
---
+++ Trajectory/Assets/src/Noah/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public Camera cam;
+    public float speed;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
