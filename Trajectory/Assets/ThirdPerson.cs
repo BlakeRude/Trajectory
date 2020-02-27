87ee01ad844771b31dde316a0697c0630c02098f
BlakeRude <60417657+BlakeRude@users.noreply.github.com>
2020-02-27 11:59:41 -0800

    ...

--- Trajectory/Assets/ThirdPerson.cs
+++ Trajectory/Assets/ThirdPerson.cs
@@ -1,29 +1,19 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPerson : MonoBehaviour
 {
-
 public Transform lookAt;
 public Transform camTransform;
-
-
-
 }
 =======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPerson : MonoBehaviour
 {
-
 public Transform lookAt;
 public Transform camTransform;
-
-
-
 }
->>>>>>> e523bb75572adea2109b2578d06f29419d905be8
