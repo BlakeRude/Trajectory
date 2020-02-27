9d1374a8a2da2b3aae0e43cc7a52002ba2d7d945
YingruoLiu <liu6033@vandals.uidaho.edu>
2020-02-26 16:16:20 -0800

    adjusted camera

---
+++ Trajectory/Assets/src/Blake/World Select Menu/SeaSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeaSelect : MonoBehaviour
+{
+    private string GameScene = "SeaScene";
+    public void StartSea()
+    {
+        //Loads scene "SeaSelect"
+        SceneManager.LoadScene(GameScene);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
