c1e0dda126fdc04386a8ceb4bd77e3ece36a5508
YingruoLiu <liu6033@vandals.uidaho.edu>
2020-03-23 01:29:04 -0700

    Created a randomizing function

---
+++ Trajectory/Assets/src/Yingruo/Scripts/RandomSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public Vector3[] positions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomNumber = Random.Range(0, positions.Length);
+        transform.position = positions[randomNumber];
+    }
+
+
+}
