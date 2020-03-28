002f913a368e133cc87be8f1aaf59c1f9248e3f2
bdRude <46767785+bdRude@users.noreply.github.com>
2020-03-28 11:11:20 -0700

    ...

---
+++ Trajectory/Assets/src/Blake/World Select Menu/SpaceSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceSelect : MonoBehaviour
+{
+    private string GameScene = "Space";
+    public void StartSpace()
+    {
+        //Loads scene
+        SceneManager.LoadScene(GameScene);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
