e0d34490518acc241f72eab219c5fcdbf827ed01
YingruoLiu <liu6033@vandals.uidaho.edu>
2020-03-29 01:51:43 -0700

    Added Camera Spawn too

---
+++ Trajectory/Assets/src/Yingruo/Scripts/Camera_Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Spawn : MonoBehaviour
+{
+    public Vector3 Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera= GameObject. Find("Player_Ship").transform.position;
+        Camera.x += 5;
+        Camera.y += 6;
+        Camera.z += -17;
+
+        transform.position = Camera;
+       
+    }
+}
