0164c586faeabb07b60cd5a27a061a4625b9fe40
YingruoLiu <liu6033@vandals.uidaho.edu>
2020-03-29 01:41:22 -0700

    Player spawn fixed

---
+++ Trajectory/Assets/src/Yingruo/Scripts/Player_Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Spawn : MonoBehaviour
+{
+    
+    public Vector3 Spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawn= GameObject. Find("Player_Ship").transform.position;
+        Spawn.x += 3;
+        Spawn.y += 2;
+
+        transform.position = Spawn;
+       
+    }
+}
