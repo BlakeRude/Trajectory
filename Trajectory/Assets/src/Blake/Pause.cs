6d384780c27c58f858c8d7c07e853f8c3ae54382
BlakeRude <46767785+bdRude@users.noreply.github.com>
2020-04-17 23:36:38 -0700

    Added Pause Menu

---
+++ Trajectory/Assets/src/Blake/Pause.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pausemenu;
+    //public GameObject Minimap;
+
+    void Start()
+    {
+        pausemenu.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                ResumeGame();
+            } else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void MainMenuButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        //Minimap.SetActive(true);
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void PauseGame()
+    {
+        //Minimap.SetActive(false);
+        pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
